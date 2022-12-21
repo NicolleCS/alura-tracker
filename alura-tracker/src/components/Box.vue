2ba4d4894a3568050f16d0a3a1cd62dd9d423959
Nicolle Santos <nicolle.csantos@outlook.com>
2022-12-21 12:14:38 -0300

    Adicionado o projeto finalizado

---
+++ alura-tracker/src/components/Box.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="box has-text-weight-bold" :style="estilos">
+    <slot></slot>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "Box",
+  data() {
+    return {
+      estilos: {
+        background: '#faf0ca'
+      }
+    }
+  }
+});
+</script>
+
+<!-- <style scoped>
+.box {
+  background: #faf0ca;
+}
+</style> -->
+ 
---
+++ alura-tracker/src/components/Tarefa.vue
@@ -0,0 +1,39 @@
+<template>
+  <Box>
+    <div class="columns">
+      <div class="column is-7">
+        {{ tarefa.descricao || "Tarefa sem descrição" }}
+      </div>
+      <div class="column">
+        <Cronometro :tempoEmSegundos="tarefa.duracaoEmSegundos" />
+      </div>
+    </div>
+  </Box>
+</template>
+
+<script lang="ts">
+import { defineComponent, PropType } from "vue";
+import ITarefa from "../interfaces/ITarefa";
+import Cronometro from "./Cronometro.vue";
+import Box from "./Box.vue";
+
+export default defineComponent({
+  name: "Tarefa",
+  props: {
+    tarefa: {
+      type: Object as PropType<ITarefa>,
+      required: true,
+    },
+  },
+  components: {
+    Cronometro,
+    Box,
+  },
+});
+</script>
+
+<style scoped>
+.box {
+  background: #faf0ca;
+}
+</style>
