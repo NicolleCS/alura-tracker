fedcffda0844185b71e15031d468e2c4ed8c46dd
Nicolle Santos <nicolle.csantos@outlook.com>
2022-12-30 13:59:47 -0300

    Alterado nome da variavel de ambiente

--- alura-tracker/src/http/index.ts
+++ alura-tracker/src/http/index.ts
@@ -1,8 +1,6 @@
 import axios, { AxiosInstance } from 'axios';
-
 const clienteHttp: AxiosInstance = axios.create({
-    baseURL: process.env.API_URL
+    baseURL: process.env.VUE_APP_API_URL
 });
-
 
 export default clienteHttp;
